7d92de3291a18969171ee40e07eaa3057ac3ee53
yu <13751762787@163.com>
2025-04-09 11:20:43 +0800

    新增员工代码＋员工分页查询+启用禁用+编辑员工+导入分类+公共字段填充

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -81,14 +81,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
         //设置当前记录的创建和修改时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+        //employee.setCreateTime(LocalDateTime.now());
+       // employee.setUpdateTime(LocalDateTime.now());
 
         //设置当前记录的创建人修改人
 
-        employee.setCreateUser(BaseContext.getCurrentId());
+     //   employee.setCreateUser(BaseContext.getCurrentId());
 
-        employee.setUpdateUser(BaseContext.getCurrentId());
+     //   employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
@@ -141,8 +141,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
 
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+     //   employee.setUpdateTime(LocalDateTime.now());
+      //  employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }
