6ee655133e7246a93590ac02fd66155e65134cc2
yu <13751762787@163.com>
2025-05-28 11:55:43 +0800

    员工管理+分类管理+公共字段填充+菜品管理+套餐管理+redis+店铺营业状态+httpclient+缓存菜品+用户端+营业额统计+用户统计+订单统计+销量排名10

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -7,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import com.github.pagehelper.Page;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -72,4 +74,12 @@ public interface OrderMapper {
      * @return
      */
     Integer countByMap(Map map);
+
+    /**
+     * 指定区间内的销量排名前十
+     * @param begin
+     * @param end
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,10 +1,12 @@
 package com.sky.service.impl;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -42,7 +45,7 @@ public class ReportServiceImpl implements ReportService {
         List<LocalDate> datelist=new ArrayList<>();
 
         datelist.add(begin);
-        while (!begin .equals(end)){
+        while (!begin .isAfter(end)){
             //计算指定日期的后一天
             begin = begin.plusDays(1);
             datelist.add(begin);
@@ -84,7 +87,7 @@ public class ReportServiceImpl implements ReportService {
         List<LocalDate> datelist=new ArrayList<>();
 
         datelist.add(begin);
-        while (!begin .equals(end)){
+        while (!begin .isAfter(end)){
             //计算指定日期的后一天
             begin = begin.plusDays(1);
             datelist.add(begin);
@@ -133,7 +136,7 @@ public class ReportServiceImpl implements ReportService {
         List<LocalDate> datelist=new ArrayList<>();
 
         datelist.add(begin);
-        while (!begin .equals(end)){
+        while (!begin .isAfter(end)){
             //计算指定日期的后一天
             begin = begin.plusDays(1);
             datelist.add(begin);
@@ -176,6 +179,27 @@ public class ReportServiceImpl implements ReportService {
                 .validOrderCount(validOrderCount)
                 .orderCompletionRate(orderCompletionRate)
 
+                .build();
+    }
+
+    /*
+     * 获取销售排行榜前10
+     */
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+
+        LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
+        LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
+
+        List<GoodsSalesDTO> salesTop10 = orderMapper.getSalesTop10(beginTime, endTime);
+        List<String> names = salesTop10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        String nameList = StringUtils.join(names, ",");
+        List<Integer> numbers = salesTop10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        String numberList = StringUtils.join(numbers, ",");
+
+
+        return SalesTop10ReportVO.builder()
+                .nameList(nameList)
+                .numberList(numberList)
                 .build();
     }
 
