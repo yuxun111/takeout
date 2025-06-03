2b3da2baf8f5952d45dba841380c23f77b00619b
yu <13751762787@163.com>
2025-06-03 21:57:46 +0800

    整體代碼

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -50,6 +51,21 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/user/user/login")
                 .excludePathPatterns("/user/shop/status");
 
+    }
+
+    /**
+     * 配置跨域
+     * @param registry
+     */
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        log.info("开始配置跨域...");
+        registry.addMapping("/**") // 所有接口
+                .allowedOriginPatterns("*") // 允许所有来源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许所有方法
+                .allowedHeaders("*") // 允许所有请求头
+                .allowCredentials(true) // 允许携带cookie
+                .maxAge(3600); // 预检请求的有效期，单位为秒
     }
 
     /**
